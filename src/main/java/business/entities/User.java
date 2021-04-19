5052e889a2583b28729e8884291fa0ef345ecc3c
cph-mn605 <cph-mn605@cphbusiness.dk>
2021-04-19 10:12:37 +0200

    test

--- src/main/java/business/entities/User.java
+++ src/main/java/business/entities/User.java
@@ -9,12 +9,12 @@ public class User
         this.password = password;
         this.role = role;
     }
-
+ 
     private int id; // just used to demo retrieval of autogen keys in UserMapper
     private String email;
     private String password; // Should be hashed and secured
     private String role;
-
+ 
     public String getEmail()
     {
         return email;
